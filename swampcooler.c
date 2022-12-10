5b610aad1dfbcfe37a79d8dbce8d4927fb58278b
5006845293 <jasminewells@nevada.unr.edu>
2022-12-09 17:21:45 -0800

    adc

--- swampcooler.c
+++ swampcooler.c
@@ -70,10 +70,10 @@ volatile unsigned char *myTIFR1  = (unsigned char *) 0x36;
 volatile unsigned char *myTIMSK1 = (unsigned char *) 0x6F;
 
 // ADC Registers 
-// volatile unsigned char* myADCSRA = (unsigned char*) 0x7A;
-// volatile unsigned char* myADCSRB = (unsigned char*) 0x7B;
-// volatile unsigned char* myADMUX  = (unsigned char*) 0x7C;
-// volatile unsigned int* myADCDATA = (unsigned int*)  0x78;
+volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
+volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
+volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
+volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
 
 // Pin change Interrupt Registers
 volatile unsigned char* myPCMSK1 = (unsigned char *) 0x6C;
@@ -99,6 +99,7 @@ const int steps_per_rev = 32;
 Stepper motor(steps_per_rev, IN1, IN3, IN2, IN4);
 int Pval = 0;
 int potVal = 0;
+int val = 0;
 
 // Functions
 
@@ -112,14 +113,20 @@ void Vent_control();
 void Write_Pin_State(volatile unsigned char* Port,unsigned char Pin,unsigned State);
 void Set_Pin(volatile unsigned char* Port, unsigned char Pin,bool INOUT);
 int Read_Water_Level();
+unsigned int adc_read(unsigned char adc_channel_num);
+void adc_init();
 
 
 void setup() {
-  Set_Pin(myDDR_B,3,Output);
-  Write_Pin_State(myDDR_B,3,OFF);
   
-  motor.setSpeed(200);
+  Set_Pin(myDDR_B,3,Output);
+  Write_Pin_State(myDDR_B,3,ON);
   Serial.begin(9600);
+  Set_Pin(myDDR_H,5,Output);
+  adc_init();
+ 
+  motor.setSpeed(200);
+  
   DHT.begin();
   servo.attach(SERVO_PIN);
   lcd.begin(16,2);
@@ -150,7 +157,14 @@ lcd.print("123456789");
 
 void loop() {
  //water
- Read_Water_Level();
+  if(Read_Water_Level()>580){
+
+   Write_Pin_State(myPORT_H,5,ON);
+  }
+  else{
+    Write_Pin_State(myPORT_H,5,OFF);
+    Serial.print("Water too low");
+  }
   //step motor
   Vent_control();
   
@@ -322,10 +336,9 @@ void Vent_control(){
 }
 
 int Read_Water_Level(){
-  int val = 0;
   
   Write_Pin_State(myPORT_B,3,ON);   //Turn the water sensor ON
-  val = analogRead(A15);            //Read the analog value from sensor
+  val = adc_read(15);               //Read the analog value from sensor
   Write_Pin_State(myPORT_B,3,OFF);  //Turn the water sensor OFF
   Serial.print("Water level: ");
   Serial.println(val);
@@ -350,4 +363,44 @@ void Write_Pin_State(volatile unsigned char* Port,unsigned char Pin,unsigned Sta
   {
     *Port |= 0x01 << Pin;         //Shoft over to pin and set to 1
   }
+}
+void adc_init()
+{
+  // setup the A register
+  *my_ADCSRA |= 0x80; // set bit   7 to 1 to enable the ADC
+  *my_ADCSRA &= 0xEF; // clear bit 5 to 0 to disable the ADC trigger mode
+  *my_ADCSRA &= 0xF7; // clear bit 5 to 0 to disable the ADC interrupt
+  *my_ADCSRA &= 0xC0; // clear bit 5 to 0 to set prescaler selection to slow reading
+  // setup the B register
+  *my_ADCSRB &= 0xF7; // clear bit 3 to 0 to reset the channel and gain bits
+  *my_ADCSRB &= 0xF8; // clear bit 2-0 to 0 to set free running mode
+  // setup the MUX Register
+  *my_ADMUX  &= 0x7F; // clear bit 7 to 0 for AVCC analog reference
+  *my_ADMUX  |= 0x40; // set bit   6 to 1 for AVCC analog reference
+  *my_ADMUX  &= 0xDF; // clear bit 5 to 0 for right adjust result
+  //@*my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
+  *my_ADMUX  &= 0xE0; // clear bit 4-0 to 0 to reset the channel and gain bits
+}
+unsigned int adc_read(unsigned char adc_channel_num)
+{
+  // clear the channel selection bits (MUX 4:0)
+  *my_ADMUX  &= 0xE0; 
+  // clear the channel selection bits (MUX 5)
+  *my_ADCSRB &= 0xF7;
+  // set the channel number
+  if(adc_channel_num > 7)
+  {
+    // set the channel selection bits, but remove the most significant bit (bit 3)
+    adc_channel_num -= 8;
+    // set MUX bit 5
+    *my_ADCSRB |= 0x08;
+  }
+  // set the channel selection bits
+  *my_ADMUX  += adc_channel_num;
+  // set bit 6 of ADCSRA to 1 to start a conversion
+  *my_ADCSRA |= 0x40;
+  // wait for the conversion to complete
+  while((*my_ADCSRA & 0x40) != 0);
+  // return the result in the ADC data register
+  return *my_ADC_DATA;
 }
