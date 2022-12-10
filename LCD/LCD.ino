2e8472347ef7494a8bbd0136a1facdb22160c1f8
Tania Jaswal <77944972+Jaswal-Tania@users.noreply.github.com>
2022-12-09 16:37:37 -0800

    lcd text

--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -1,11 +1,14 @@
 
 #include <LiquidCrystal.h>
-
+#include <water.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup()
 {
-  lcd.print("hello, world!");
+  lcd.begin(16, 2);
+  lcd.print("Water level: ");
 }
 
-void loop() {}
+void loop() {
+  lcd.setCursor(0, 1);
+}
